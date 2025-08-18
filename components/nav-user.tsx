3fe40d8fee7a76010e0b656e1c4080d418a00c5e
Janos Takacs <takacsjan@gmail.com>
2025-08-18 14:29:51 +0200

    Fix user data type and initials

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -14,15 +14,9 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar"
 import { LogoutLink } from "@/components/logout-link"
+import { ICurrentUser } from "@/hooks/use-current-user"
 
-export function NavUser({
-  user
-}: {
-  user: {
-    name: string
-    email: string
-  }
-}) {
+export function NavUser({ user }: { user: ICurrentUser }) {
   const { isMobile } = useSidebar()
 
   return (
@@ -36,7 +30,7 @@ export function NavUser({
             >
               <Avatar className="h-8 w-8 rounded-lg grayscale">
                 <AvatarImage src={undefined} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg">{user.initials}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{user.name}</span>
@@ -55,7 +49,7 @@ export function NavUser({
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={undefined} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">{user.initials}</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.name}</span>
--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -2,7 +2,18 @@ import { createClient } from "@/lib/supabase/client"
 import { UserMetadata } from "@supabase/supabase-js"
 import { useEffect, useState } from "react"
 
-export const useCurrentUser = () => {
+export interface ICurrentUser {
+  name: string
+  firstname: string
+  lastname: string
+  initials: string
+  email: string
+  isAdmin: boolean
+  phone: string
+  roles: string[]
+}
+
+export const useCurrentUser = (): ICurrentUser => {
   const [userMeta, setUserMeta] = useState<UserMetadata | null>(null)
 
   useEffect(() => {
@@ -21,9 +32,12 @@ export const useCurrentUser = () => {
 
   return {
     name: userMeta?.user ? `${userMeta.user.lastname} ${userMeta.user.firstname}` : "?",
-    email: userMeta?.user?.email || "?",
+    firstname: userMeta?.user.firstname ?? "?",
+    lastname: userMeta?.user.lastname ?? "?",
+    initials: userMeta?.user ? `${userMeta.user.lastname[0]}${userMeta.user.firstname[0]}` : "?",
+    email: userMeta?.user?.email ?? "?",
     isAdmin: userMeta?.user?.isAdmin || false,
-    phone: userMeta?.user?.phone || "?",
-    roles: userMeta?.user?.roles || []
+    phone: userMeta?.user?.phone ?? "?",
+    roles: userMeta?.user?.roles ?? []
   }
 }
