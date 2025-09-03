52d74ba0062186889b05ad858a8f068f8be60933
Janos Takacs <takacsjan@gmail.com>
2025-09-03 18:56:58 +0200

    Add active state for main navigation

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -10,6 +10,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem
 } from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation"
 
 export function NavMain({
   items
@@ -20,6 +21,8 @@ export function NavMain({
     icon?: Icon
   }[]
 }) {
+  const pathname = usePathname()
+
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
@@ -41,7 +44,12 @@ export function NavMain({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5" tooltip={item.title}>
+              <SidebarMenuButton
+                isActive={pathname === item.url}
+                asChild
+                className="data-[slot=sidebar-menu-button]:!p-1.5"
+                tooltip={item.title}
+              >
                 <a href={item.url}>
                   {item.icon && <item.icon />}
                   <span>{item.title}</span>
