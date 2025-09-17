85b71cd1d0f9b0d4b8e05483d9edf78650c55e07
Janos Takacs <takacsjan@gmail.com>
2025-09-17 16:43:17 +0200

    Add speculationrules script to body to use pre-rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import { ThemeProvider } from "next-themes"
 import "./globals.css"
 import { Toaster } from "@/components/ui/sonner"
+import Script from "next/script"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,11 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
         <Toaster position="top-right" expand richColors closeButton duration={3000} />
+        <Script type="speculationrules">
+          {JSON.stringify({
+            prerender: [{ source: "document", eagerness: "moderate" }]
+          })}
+        </Script>
       </body>
     </html>
   )
