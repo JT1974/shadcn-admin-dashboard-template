2d24ec3fd4ae38fc9f9f6d72e7d87e5cea66fd0f
Janos <takacsjan@gmail.com>
2025-10-28 09:52:12 +0100

    Fix quotation error page link

--- app/dashboard/quotations/error.tsx
+++ app/dashboard/quotations/error.tsx
@@ -8,9 +8,9 @@ function Error({ error: { message } }: { error: { message?: string } }) {
     <div className="flex size-full flex-col items-center justify-center gap-4">
       <h2 className="text-4xl font-semibold text-red-600 sm:text-6xl dark:text-red-500">Sorry...</h2>
       <h3 className="text-center text-2xl sm:text-3xl">{message}</h3>
-      <Link href="/dashboard/quotations" className="flex gap-2 hover:text-blue-700 dark:hover:text-blue-400">
+      <Link href="/dashboard" className="flex gap-2 hover:text-blue-700 dark:hover:text-blue-400">
         <IconArrowBack />
-        Back to quotations
+        Back to the dashboard
       </Link>
     </div>
   )
